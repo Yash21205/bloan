9fbd0512cfa9671a554952331f3132f9fae277a8
Yash21205 <yash21205@gmail.com>
2021-08-06 17:00:19 +0530

    Revert "C61"
    This reverts commit 86f29399827eb0f5964555b88ee7c955ff6fed69.

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Landing from "./components/BasicComponents/Landing"
-import { NavigationContainer } from '@react-navigation/native';
-import Drawernavigator from './components/Navigation/Drawnavigator'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawernavigator>
-        
-      </Drawernavigator>
-    </NavigationContainer>
-     
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
